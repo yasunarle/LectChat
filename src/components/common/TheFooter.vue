b6b2e70f50f00897b6ea7c8b0b8578328e83c94e
Yasunari t <u7df25c588m737@gmail.com>
2020-07-30 22:36:50 +0900

    🚣chore: fixed some text in footer

--- src/components/common/TheFooter.vue
+++ src/components/common/TheFooter.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="footer">
-    <p>Copyright © 2020 Yasunarle</p>
+    <p>
+      このサイトのソースコードはで公開
+      <a href="https://github.com/yasunarle/LectChat">MIT License</a> されています。
+    </p>
+    <p>
+      不具合報告やお問い合わせは、作者
+      <a href="https://twitter.com/u7df25c588m737">Twitter アカウント</a> へとお願いいたします。
+    </p>
+    <p>
+      © 2020 lectchat.com by
+      <a href="https://twitter.com/u7df25c588m737">@yasunarle</a>
+    </p>
   </div>
 </template>
 <style lang="scss">
 .footer {
   min-height: 100px;
   width: 100%;
+  padding-top: 20px;
   text-align: center;
   background: #475050;
   color: #ffffff;
+  a {
+    text-decoration: underline;
+  }
 }
 </style>
